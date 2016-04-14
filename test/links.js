58feab3cf3dac754fdeadcd8665de6f702df07fc
ansteh <stehle.andre@googlemail.com>
2016-04-14 22:30:15 +0200

    update & testbed

--- test/links.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-  'https://github.com/ansteh/url-graph',
-  'https://github.com/lodash/lodash',
-  'https://github.com/ansteh/shape-array',
-  'https://github.com/sindresorhus/got',
-  'https://github.com/ansteh/shape-json'
-];
--- test/spec/index.js
+++ test/spec/index.js
@@ -1,13 +1,16 @@
 'use strict';
-const graph = require('../../index.js');
-const links = require('../links.js');
+const lib     = require('../../index.js');
+const testbed = require('../testbed.js');
+const _       = require('lodash');
 
 describe("example", function() {
-  it("readme intro", function() {
-    //console.log(links, graph);
-    console.log(JSON.stringify(graph.treefy(links)));
-    /*let person = get('person');
-    let scheme = shape.scheme(person.scheme);
-    expect(scheme(person.input)).toEqual(person.output);*/
+  _.forEach(testbed, function(test){
+    it(test.name, function() {
+      let operation = lib[test.options.instruction];
+      let input = test.options.input;
+      let output = test.options.output;
+
+      expect(operation(input)).toEqual(output);
+    });
   });
 });
